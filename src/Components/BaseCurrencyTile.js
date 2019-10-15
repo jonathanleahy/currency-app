b5abb0cc3158ae4058af0f34e35785d634b907b4
Jon <jonathanl@newtimesystems.com>
2019-10-15 07:34:27 +0100

    refactor main page and convert to smaller components

---
+++ src/Components/BaseCurrencyTile.js
@@ -0,0 +1,27 @@
+fimport React from "react";
+import {clsCountryCodes} from "../Classes/clsCountryCodes";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 250px;
+    display: flex;
+    position: relative;
+`
+
+class CountryFlag extends React.Component {
+
+    render() {
+        return (
+            <Container>
+                <img
+                    src={clsCountryCodes.flagImage(this.props.countrycode)}
+                    alt={this.props.countrycode}
+                    width={'100%'}
+                />
+            </Container>
+        );
+    }
+
+}
+
+export default CountryFlag;
